44661b919114d3f16aed682dd03ccc2585e31bb1
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2026-05-04 09:53:02 +0600

    development url

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,18 +2,30 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/skillsphere");
-const db = client.db();
+const client = new MongoClient(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/skillsphere",
+);
+const db = client.db("skill-sphere");
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db),
-    emailAndPassword: {
-        enabled: true
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://skill-sphere-edu.vercel.app",
+  trustedOrigins: [
+    "https://skill-sphere-edu.vercel.app",
+    "http://localhost:3000",
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID || "dummy",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "dummy",
-        }
-    }
+  },
 });
